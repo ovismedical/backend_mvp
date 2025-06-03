71c019b979ef53d7c630aecd922d52e630dbab51
Evan Ning <evanning0206@gmail.com>
2025-06-03 10:53:50 -0400

    fix

--- app/questions.py
+++ app/questions.py
@@ -15,12 +15,12 @@ async def get_questions(db = Depends(get_db)):
     raise HTTPException(status_code=404, detail="Database empty")
 
 @questionsrouter.get("/submitquestions")
-async def submit(answers):
+async def submit(answers, user = Depends(get_user),):
     filename = "results"+(datetime.now().strftime("%m%d%Y_%H%M%S"))
     with open ("answers/"+filename, 'w') as f:
         json.dump(answers, f)
     return ("Answers saved at " + filename)
 
 @questionsrouter.get("/getstreak")
-def get_streak(user = Depends(get_user), db=Depends(get_db)):
+def get_streak(user = Depends(get_user)):
     return ({"streak" : user["streak"]})
